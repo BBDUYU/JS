1f2fe9f3677e8c604dbd7bfc6008076ee260d518
BBDUYU <tmdwhddh@gmail.com>
2025-03-06 23:14:21 +0900

    navheight

--- Scroll/app.js
+++ Scroll/app.js
@@ -37,3 +37,23 @@ window.addEventListener("scroll", function () {
 //smooth scroll
 
 //select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        //prevent default
+        e.preventDefault();
+        //navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        //calculate height
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
